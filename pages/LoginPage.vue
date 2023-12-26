3b6d13d18dc74095100e2ed69a6835620c48fb71
syuichi <n-syuichi@users.noreply.github.com>
2023-12-26 22:12:42 +0900

    [modify] client_id

---
+++ pages/LoginPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-btn class="bg-emerald-600 text-white" v-on:click="login"> Login </v-btn>
+</template>
+  
+<script setup lang="ts">
+
+const login = () => {
+    const response_type = 'code';
+    const client_id = process.env.LINE_CHANNEL_ID!;
+    const redirect_uri = 'https://blog.at-himawari.com/login/line/callback'; //設定したコールバック関数をURLエンコードしたもの
+    const state = '12345abcde';//一旦ここはテキトー
+    const scope = 'profile%20openid';
+    const nonce = '09876xyz';//一旦ここはテキトー
+
+    window.location.href = 'https://access.line.me/oauth2/v2.1/authorize?response_type=' + response_type +
+        '&client_id=' + client_id +
+        '&redirect_uri=' + redirect_uri +
+        '&state=' + state +
+        '&scope=' + scope +
+        '&nonce=' + nonce;
+}
+
+</script>
+  
