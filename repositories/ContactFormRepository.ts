54b0ef69fa0ac905231c641f363b6280e0d40c32
syuichi <n-syuichi@users.noreply.github.com>
2024-01-14 20:49:39 +0900

    [modify] set contact form url

--- repositories/ContactFormRepository.ts
+++ repositories/ContactFormRepository.ts
@@ -3,9 +3,13 @@ import axios from "axios";
 export default class ContactFormRepository{
   static async postMessage(body: ContactRequestType) {
     console.log(body);
-        await axios.post(
-          process.env.LINK_CONTACT_FORM!,
-            JSON.stringify(body)
-          );
+    await useFetch("https://wdruieg1nl.execute-api.ap-northeast-1.amazonaws.com/himawari-blog-contact-form", {
+      method: 'POST',
+      body: body,
+  });
+        // await axios.post(
+        //   process.env.LINK_CONTACT_FORM!,
+        //     JSON.stringify(body)
+        //   );
     }
 }
