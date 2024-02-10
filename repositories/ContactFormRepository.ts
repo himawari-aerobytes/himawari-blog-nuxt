a5a049451b0a036ba60fb2ec9be9b3bf7e9bc25b
syuichi <n-syuichi@users.noreply.github.com>
2024-02-10 19:57:27 +0900

    [modify] envから取得

--- repositories/ContactFormRepository.ts
+++ repositories/ContactFormRepository.ts
@@ -1,14 +1,12 @@
-import axios from "axios";
-
 export default class ContactFormRepository{
   static async postMessage(body: ContactRequestType) {
     console.log(body);
     const runtimeConfig = useRuntimeConfig();
-    // API_CONTACT_FORMからAPIのURLを取得
+    // CONTACT_LINKからAPIのURLを取得
     const apiContactForm = runtimeConfig.public.contactLink;
     console.log(apiContactForm);
 
-    // API_CONTACT_FORMにPOSTリクエストを送信
+    // CONTACT_LINKにPOSTリクエストを送信
     await useFetch(apiContactForm, {
       method: 'POST',
       body: body,
